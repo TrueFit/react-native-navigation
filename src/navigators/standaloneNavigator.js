35d1e8f933c108ee3050335de1597e591d92252d
Josh Gretz <jgretz@truefit.io>
2017-09-11 14:37:06 -0400

    handle reset possibilities more fully

--- src/navigators/standaloneNavigator.js
+++ src/navigators/standaloneNavigator.js
@@ -1,70 +1,94 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'; // eslint-disable-line
 import {NavigationActions} from 'react-navigation';
 
 const INITIAL_ROUTE = 'INITIAL_ROUTE';
 
+const navigate = action =>
+  ({
+    index: 0,
+    routes: [
+      {
+        routeName: action.routeName,
+        key: action.routeName,
+        ...action.params,
+      },
+    ],
+  });
+
 const StandaloneRouter = routes =>
-({
-  getStateForAction: action => {
-    switch (action.type) {
-      case INITIAL_ROUTE:
-        return {
-          index: 0,
-          routes: [
-            {
-              routeName: action.payload.path,
-              key: action.payload.path,
-              ...action.payload.params,
-            },
-          ],
-        };
+  ({
+    getStateForAction: action => {
+      switch (action.type) {
+        case INITIAL_ROUTE:
+          return {
+            index: 0,
+            routes: [
+              {
+                routeName: action.payload.path,
+                key: action.payload.path,
+                ...action.payload.params,
+              },
+            ],
+          };
 
-      case NavigationActions.NAVIGATE:
-      case NavigationActions.RESET:
-        return {
-          index: 0,
-          routes: [
-            {
-              routeName: action.routeName,
-              key: action.routeName,
-              ...action.params,
-            },
-          ],
-        };
+        case NavigationActions.NAVIGATE:
+          return navigate(action);
 
-      default:
-        return null;
-    }
-  },
+        case NavigationActions.RESET: {
+          if (action.routeName) {
+            return navigate(action);
+          }
 
-  getActionForPathAndParams: (path, params) =>
-    ({
-      type: INITIAL_ROUTE,
-      payload: {path, params},
-    }),
+          const innerAction = action.actions > 0 ? action.actions[0] : null;
+          if (!innerAction) {
+            return null;
+          }
 
-  getPathAndParamsForState: state =>
-    ({
-      path: state.routes[state.index].routeName,
-    }),
+          return {
+            index: 0,
+            routes: [
+              {
+                routeName: innerAction.routeName,
+                key: innerAction.routeName,
+                ...innerAction.params,
+              },
+            ],
+          };
+        }
 
-  getComponentForState: state => {
-    const routeName = state.routes[state.index].routeName;
-    return (routes[routeName] || {}).screen || null;
-  },
+        default:
+          return null;
+      }
+    },
 
-  getComponentForRouteName: routeName =>
-    (routes[routeName] || {}).screen || null,
-});
+    getActionForPathAndParams: (path, params) =>
+      ({
+        type: INITIAL_ROUTE,
+        payload: {path, params},
+      }),
+
+    getPathAndParamsForState: state =>
+      ({
+        path: state.routes[state.index].routeName,
+      }),
+
+    getComponentForState: state => {
+      const routeName = state.routes[state.index].routeName;
+      return (routes[routeName] || {}).screen || null;
+    },
+
+    getComponentForRouteName: routeName =>
+      (routes[routeName] || {}).screen || null,
+  });
 
 export const StandaloneNavigator = routes => {
-  const router = StandaloneRouter(routes);
+  const router = StandaloneRouter(routes); // eslint-disable-line
 
   class Navigator extends Component {
     static router = router;
 
     render() {
-      const Component = router.getComponentForState(this.props.navigation.state);
+      const Component = router.getComponentForState(this.props.navigation.state); // eslint-disable-line
 
       return <Component />;
     }
